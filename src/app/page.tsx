0d85d0a032cc1fff072026eb2e48035591f0094d
dennisDev77 <diniscamela5@gmail.com>
2025-04-01 15:32:45 +0100

    feat: 🎸 Adiciona Algumas Rotas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <main className="">
-        <h1>Bem vindo a Home</h1>
+        <h1 className='text-blue-100 bg-gray-300'>Bem vindo a Home</h1>
     </main>
   );
 }
