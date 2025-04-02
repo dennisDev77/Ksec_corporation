dcb6cbee957342ce0ce045f23d6a6cc6f35499b9
dennisDev77 <diniscamela5@gmail.com>
2025-04-02 23:26:24 +0100

    feat: 🎸 Adiciona Tailwind v3
    Adicionamos o tailwind v3 porque o v4 nao funcionava corretamente, ele
    falhava em alguns valores como px-4
    
    BREAKING CHANGE: 🧨 Removemos v4

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}" // se estiver utilizando o app router do Next.js 13+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
