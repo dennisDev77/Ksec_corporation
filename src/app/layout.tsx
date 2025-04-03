519c3fb0586d49859b85bc568267c9414bd144a3
dennisDev77 <diniscamela5@gmail.com>
2025-04-03 18:32:38 +0100

    feat: 🎸 Adiciona Font Roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.scss";
 import Header from "@/components/Header/Header";
 
 //Adicionando as configuracoes das fontes do google
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 //Adicionando metadados para o site
@@ -26,7 +22,7 @@ export default function RootLayout({ children }: Readonly<{
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${roboto.variable} antialiased`}>
         <Header />
         {children}
         
