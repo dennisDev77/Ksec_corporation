a941f9596c33e3d453734ab1968c538999adcdbb
dennisDev77 <diniscamela5@gmail.com>
2025-04-04 01:44:28 +0100

    feat: 🎸 Remove Banner da Home
    Com Banner presente, aparentava esta muito poluido nao estar
    personalizada, ou melhor, a imagem precisa estar ambientalizada
    
    BREAKING CHANGE: 🧨 Remove Banner
    
    ✅ Closes: #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //Import components
 import BackgroundHome from './home/BackgroundHome'
-import Banner from './home/Banner'
+// import Banner from './home/Banner'
 import Button from '@/components/Forms/Button'
 import styles from '@/components/Header/Header.module.scss'
 
@@ -22,12 +22,12 @@ export default function Home() {
         </div>
 
           {/* Content Home */}
-        <div className=" Container relative flex md:flex-row flex-col justify-between items-center text-zinc-200 z-20 w-full md:gap-0 gap-4">
+        <div className=" Container relative flex items-center justify-center text-zinc-200 z-20 w-full md:gap-0 gap-4">
 
-          <div className='pt-20 md:max-w-2xl flex flex-col gap-4 items-start justify-center'>
-              <h2 className="text-5xl font-bold text-zinc-200"> Sua Seguranca <span className='text-[#078a42]'>Online</span> e a Chave da Sua Tranquilidade <span className='text-[#078a42]'>Offline</span>.</h2>
+          <div className='md:pt-32 pt-16 md:max-w-4xl flex flex-col gap-4 items-center justify-center'>
+              <h2 className="text-5xl font-bold text-zinc-200 text-center"> Sua Seguranca <span className='text-[#078a42]'>Online</span> é a Chave da Sua Tranquilidade <span className='text-[#078a42]'>Offline</span>.</h2>
 
-              <p className='text-base text-zinc-400 text-justify font-light'>Proteger a sua empresa, dolor sit amet consectetur adipisicing elit. Incidunt illum repudiandae, suscipit harum, atque recusandae voluptas earum quidem ipsum expedita iste! Amet quae possimus fugiat dolore! Dolores necessitatibus consectetur labore!</p>
+              <p className='text-lg text-zinc-400 text-center font-light'>Proteger a sua empresa, dolor sit amet consectetur adipisicing elit. Incidunt illum repudiandae, suscipit harum, atque recusandae voluptas earum quidem ipsum expedita iste! Amet quae possimus fugiat dolore! Dolores necessitatibus consectetur labore quidem ipsum expedita iste! Amet quae possimus fugiat dolore! Dolores necessitatibus consectetur labore!</p>
   
 
               <div className='pt-4 flex flex-wrap justify-start gap-10 items-center'>
@@ -36,9 +36,9 @@ export default function Home() {
               </div>
           </div>
 
-          <div className="pt-20 md:max-w-xl">
+          {/* <div className="pt-20 md:max-w-xl">
                 <Banner/>
-          </div>
+          </div> */}
         </div>
        </section>
 
